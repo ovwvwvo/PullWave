e55f3ecf5331de504cca9bcc3f211ad092c81c10
rawer <rawer.dev@gmail.com>
2017-06-12 22:23:09 +0800

    detailView complete

--- app/src/main/java/com/ovwvwvo/pullwave/activity/DetailActivity.java
+++ app/src/main/java/com/ovwvwvo/pullwave/activity/DetailActivity.java
@@ -1,15 +1,16 @@
 package com.ovwvwvo.pullwave.activity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
-import com.github.mikephil.charting.charts.CombinedChart;
-import com.github.mikephil.charting.data.CombinedData;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.ovwvwvo.pullwave.R;
 import com.ovwvwvo.pullwave.model.DataResponse;
 import com.ovwvwvo.pullwave.presenter.DetailPresenter;
@@ -28,7 +29,7 @@ import butterknife.ButterKnife;
 public class DetailActivity extends AppCompatActivity implements DetailView {
 
     @BindView(R.id.combinedChart)
-    CombinedChart chart;
+    BarChart chart;
 
     public static final String WORD = "word";
 
@@ -48,17 +49,28 @@ public class DetailActivity extends AppCompatActivity implements DetailView {
 
     @Override
     public void onLoadSuccess(DataResponse response) {
-        List<Entry> entries = new ArrayList<>();
+        List<BarEntry> entries = new ArrayList<>();
+        BarEntry entry;
         for (int i = 0; i < response.getModels().size(); i++) {
-            entries.add(new Entry(i, response.getModels().get(i).getV()));
+            entry = new BarEntry(i, response.getModels().get(i).getV());
+            entries.add(entry);
         }
-        LineDataSet dataSet = new LineDataSet(entries, "Label");
-        dataSet.setColor(Color.RED);
-        dataSet.setValueTextColor(Color.BLUE);
-        LineData lineData = new LineData(dataSet);
-        CombinedData c = new CombinedData();
-        c.setData(lineData);
-        chart.setData(c);
+        BarDataSet set = new BarDataSet(entries, getString(R.string.exponent));
+        set.setColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        BarData barData = new BarData(set);
+        chart.setData(barData);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setLabelCount(15, true);
+        xAxis.setLabelRotationAngle(60);
+        xAxis.setValueFormatter((value, axis) -> response.getModels().get((int) value).getDate().substring(5));
+
+        Description description = new Description();
+        description.setEnabled(false);
+        chart.setDescription(description);
+
         chart.invalidate();
     }
 }
